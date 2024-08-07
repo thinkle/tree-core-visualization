b755f7af6da6dc840f1adf17334afb30bb8f5314
Tom Hinkle <tmhinkle@gmail.com>
2024-08-07 09:52:07 -0400

    Oops -- svelte-prism fix actually broke things

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import path from "path";
 export default defineConfig({
   plugins: [svelte()],
-
-  build: {
-    rollupOptions: {
-      external: ["svelte-prism"], // Exclude svelte-prism if it's causing issues
-    },
-  },
 });
